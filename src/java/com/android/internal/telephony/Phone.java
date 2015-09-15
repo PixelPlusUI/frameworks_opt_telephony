93d12dc252b14d8305784736ec55c14283b5784a
Ravindra <c_rthat@codeaurora.org>
2015-09-15 18:19:51 +0530

    Emergency Number Implementation for SS & DS
    Emergency Number Implementation for Single standby
    and Dual Standby
    
    Change-Id: Id50e26ef8f05b760ad79b207193ab5183a222bd9
    CRs-Fixed: 2178453

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -51,6 +51,7 @@ import android.telephony.CellLocation;
 import android.telephony.ClientRequestStats;
 import android.telephony.DataFailCause;
 import android.telephony.ImsiEncryptionInfo;
+import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
 import android.telephony.PhysicalChannelConfig;
 import android.telephony.RadioAccessFamily;
@@ -4230,5 +4231,9 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
             pw.flush();
             pw.println("++++++++++++++++++++++++++++++++");
         }
+    }
+
+    public boolean isEmergencyNumber(String address) {
+        return PhoneNumberUtils.isEmergencyNumber(getSubId(), address);
     }
 }
