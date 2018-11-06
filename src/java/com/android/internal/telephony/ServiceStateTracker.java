5fddd25fc6072e9d85ffddebd2c2893d93f8b5bd
qiongz <qiongz@codeaurora.org>
2018-11-06 14:22:34 +0800

    Fix non-roaming overlay not work issue.
    Roaming type is not expected if carrier config has not started
    to load when SST tries to update roaming state. And if service
    state is not change after carrier config loaded, the roaming
    type will keep to be improper.
    
    To fix this issue, add a flag that indicates whether carrier
    config loaded. And poll state done again after carrier config
    loading finished.
    
    Change-Id: Idcc0a5d7469fdcac98dd38311b655e5b478577d1
    CRs-Fixed: 2344948

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -323,6 +323,7 @@ public class ServiceStateTracker extends Handler {
     private int mPrevSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
 
     private boolean mImsRegistered = false;
+    private boolean mCarrierConfigLoaded = false;
 
     @UnsupportedAppUsage
     private SubscriptionManager mSubscriptionManager;
@@ -2398,7 +2399,7 @@ public class ServiceStateTracker extends Handler {
              * The test for the operators is to handle special roaming
              * agreements and MVNO's.
              */
-            boolean roaming = (mGsmRoaming || mDataRoaming);
+            boolean roaming = (mGsmRoaming || mDataRoaming) && mCarrierConfigLoaded;
 
             // for IWLAN case, data is home. Only check voice roaming.
             if (mNewSS.getRilDataRadioTechnology() == ServiceState.RIL_RADIO_TECHNOLOGY_IWLAN) {
@@ -4693,6 +4694,9 @@ public class ServiceStateTracker extends Handler {
         // Load the ERI based on carrier config. Carrier might have their specific ERI.
         mEriManager.loadEriFile();
         mCdnr.updateEfForEri(getOperatorNameFromEri());
+
+        mCarrierConfigLoaded = true;
+        pollState();
 
         updateLteEarfcnLists(config);
         updateReportingCriteria(config);
