be244ad582eba33ec7fa84f817f9d85383046b6f
baalajimaestro <me@baalajimaestro.me>
2020-05-31 19:52:03 +0530

    Revert "Fix wrong APN list issue for multimode SIM cards."
    This reverts commit 64456ec735c5a76b12ccdce575e6b43d44440660.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I78fd48cbd042e08df7624b84cd54e0732860d405

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -83,7 +83,6 @@ import com.android.internal.telephony.uicc.IccCardApplicationStatus.AppType;
 import com.android.internal.telephony.uicc.IccFileHandler;
 import com.android.internal.telephony.uicc.IccRecords;
 import com.android.internal.telephony.uicc.IsimRecords;
-import com.android.internal.telephony.uicc.SIMRecords;
 import com.android.internal.telephony.uicc.UiccCard;
 import com.android.internal.telephony.uicc.UiccCardApplication;
 import com.android.internal.telephony.uicc.UiccController;
@@ -4071,10 +4070,6 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
             return ((RIL) mCi).getHalVersion();
         }
         return RIL.RADIO_HAL_VERSION_UNKNOWN;
-    }
-
-    public SIMRecords getSIMRecords() {
-        return null;
     }
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
